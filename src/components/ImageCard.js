829f2f246cce3812bcbf2b33893834f3acb668b3
Nathan Melby <nathan@Nathans-Mac-mini.local>
2019-09-12 18:29:04 -0500

    built out components and rough css.

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,15 +1,68 @@
 import React from "react";
+import styled from 'styled-components';
+
+
+const SiteContainer = styled.div`
+    display: flex;
+    flex-direction:column;
+    justify-content: center;
+    background-color: #EAE6DA;
+    font-family: 'Lato', sans-serif;
+    padding-bottom: 10vh;
+    padding-top: 5vh;
+
+
+
+`;
+const CardContainer = styled.div`
+
+
+`;
+
+const CardImage = styled.img`
+
+  width:50%;
+  height: auto;
+  margin-left: auto;
+  margin-right:auto;
+
+  
+`;
+
+const CardTitle = styled.h1`
+font-size:3rem;
+
+`;
+
+const CardDate = styled.h2`
+font-size: 1rem;
+
+`;
+
+const CardPhotographer = styled.h3`
+font-size: 3 rem;
+
+`;
+
+const CardDescription = styled.p`
+font-size: 2 rem;
+margin-right: 15%;
+margin-left: 15%;
+line-height: 2;
+
+`;
 
 const ImgCard = props => {
   return (
-    <div className="img-card">
-        <h1 className="image-title">{props.title}</h1>
-        <img className='daily-image' alt="Outer Space" src = {props.url} />
-        
-        <h2 className='today-date'>{props.date}</h2>
-        <h3 className='copyright-holder'>{props.copyright}</h3>
-        <p className='explanation'>{props.explanation}</p>
-    </div>
+    <SiteContainer>
+      <CardContainer>
+        <CardDate>Today's Date: {props.date}</CardDate>
+        <CardTitle>{props.title}</CardTitle>
+        <CardImage alt="Outer Space" src = {props.url} />
+        <CardPhotographer>Photo by: {props.copyright}</CardPhotographer>
+        <CardDescription>{props.explanation}</CardDescription>
+      </CardContainer>
+    </SiteContainer>
   );
 };
 
